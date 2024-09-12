388523d76384862138783080b7ffed84018c1495
DohaData <doha.kaddaf@gmail.com>
2024-09-12 21:40:16 +0200

    finish up navrbar and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import SignUpPage from "./pages/SignUpPage/SignupPage";
+// import LogoutPage from "./pages/LogoutPage/LogoutPage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
+// import HabitsPage from "./pages/HabitsPage/HabitsPage";
+// import HabitsTrackerPage from "./pages/HabitsTrackerPage/HabitsTrackerPage";
+// import YourHabitsPage from "./pages/YourHabitsPage/YourHabitsPage";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        {/* <Route path="/logout" element={<LogoutPage />} /> */}
+        <Route path="/profile" element={<ProfilePage />} />
+        {/* <Route path="/habits" element={<HabitsPage />} />
+        <Route path="/habits-tracker" element={<HabitsTrackerPage />} />
+        <Route path="/your-habits" element={<YourHabitsPage />} /> */}
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
