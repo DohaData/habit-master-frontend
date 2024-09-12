d23e476f3b41a88f739aba0c3e7421bcff37ef68
DohaData <doha.kaddaf@gmail.com>
2024-09-12 21:45:14 +0200

    try fix deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import SignUpPage from "./pages/SignUpPage/SignupPage";
+import HomePage from "./pages/HomePage/HomePage.jsx";
+import LoginPage from "./pages/LoginPage/LoginPage.jsx";
+import SignUpPage from "./pages/SignUpPage/SignupPage.jsx";
 // import LogoutPage from "./pages/LogoutPage/LogoutPage";
-import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage.jsx";
 // import HabitsPage from "./pages/HabitsPage/HabitsPage";
 // import HabitsTrackerPage from "./pages/HabitsTrackerPage/HabitsTrackerPage";
 // import YourHabitsPage from "./pages/YourHabitsPage/YourHabitsPage";
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/Navbar/Navbar.jsx";
 
 function App() {
   return (
